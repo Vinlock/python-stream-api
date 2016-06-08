eb87366abeaa6b13426b38cb1bbc51129dc195a9
Vinlock <dak@pvpallday.com>
2016-06-07 21:44:24 -0700

    Fixed Twitch Bio

--- app.py
+++ app.py
@@ -125,7 +125,7 @@ while(True):
         starter.cut()
     else:
         starter.cut(9)
-    if starter.output_to_json("../streams.json"):
+    if starter.output_to_json("/var/www/vhosts/pvpallday.com/laravel/public/234c12f13-streams/streams.json"):
         now = time.strftime("%c")
         print(">>> Generated JSON ---", "Current time %s"  % time.strftime("%c"))
 
